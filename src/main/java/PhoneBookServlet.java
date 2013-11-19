fdac2a1ea3af9c477f573b97f1a1d1f2696c172f
Admin <Admin@home-f39f89e10d>
2013-11-19 17:59:48 +0200

    Commit from JBoss Tools

--- src/main/java/PhoneBookServlet.java
+++ src/main/java/PhoneBookServlet.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class PhoneBookServlet
  */
-@WebServlet(name="PhoneBookServlet", urlPatterns={"/"})
+@WebServlet(name="PhoneBookServlet", urlPatterns={"/adr"})
 public class PhoneBookServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
