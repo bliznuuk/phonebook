fa48121d74a5f5155f61c9d69e23931f9bce81e6
Admin <Admin@home-f39f89e10d>
2013-11-28 13:03:13 +0200

    21

--- src/main/java/resound/phonebook/testSql.java
+++ src/main/java/resound/phonebook/testSql.java
@@ -1,6 +1,7 @@
 package resound.phonebook;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 import javax.naming.Context;
@@ -14,11 +15,13 @@ public class testSql {
 	private static final String USERNAME = System.getenv("OPENSHIFT_MYSQL_DB_USERNAME");
 	private static final String PASSWORD = System.getenv("OPENSHIFT_MYSQL_DB_PASSWORD");
 	private static final String URL = System.getenv("OPENSHIFT_MYSQL_DB_URL");
+	private static final String DBNAME = "phonebook3";
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		String dsc = "java:comp/env/jdbc/address";
-		Connection connection;
-		try {
+		Connection connection = DriverManager.getConnection(
+				"jdbc:mysql://" + HOST + ":" + PORT + "/" + DBNAME, USERNAME, PASSWORD);
+/*		try {
 			Context context = new InitialContext();
 			DataSource dataSource = (DataSource) context.lookup(dsc);
 			connection = dataSource.getConnection();
@@ -26,7 +29,7 @@ public class testSql {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+*/		
 	}
 
 }
