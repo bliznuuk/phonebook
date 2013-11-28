5b75acc10feed63ef990d90f832aca466b8dd236
Admin <Admin@home-f39f89e10d>
2013-11-28 11:25:56 +0200

    Commit from JBoss Tools

--- src/main/java/resound/phonebook/testSql.java
+++ src/main/java/resound/phonebook/testSql.java
@@ -9,6 +9,11 @@ import javax.naming.NamingException;
 import javax.sql.DataSource;
 
 public class testSql {
+	private static final String HOST = System.getenv("OPENSHIFT_MYSQL_DB_HOST");
+	private static final String PORT = System.getenv("OPENSHIFT_MYSQL_DB_PORT");
+	private static final String USERNAME = System.getenv("OPENSHIFT_MYSQL_DB_USERNAME");
+	private static final String PASSWORD = System.getenv("OPENSHIFT_MYSQL_DB_PASSWORD");
+	private static final String URL = System.getenv("OPENSHIFT_MYSQL_DB_URL");
 
 	public static void main(String[] args) {
 		String dsc = "java:comp/env/jdbc/address";
