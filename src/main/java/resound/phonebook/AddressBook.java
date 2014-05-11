cfe1364e4794e9449fa2864b05f470ef34dbddc8
Vitaliy Bliznyuk <bliznuuk@gmail.com>
2014-05-11 14:24:25 +0300

    pom.xml -> add connection-pool and connection-dbcp to dependencies; AdderessBook -> contacts save in db; AddressBookServlet -> change throws declaration, add catch to handle().

--- src/main/java/resound/phonebook/AddressBook.java
+++ src/main/java/resound/phonebook/AddressBook.java
@@ -1,81 +1,131 @@
 package resound.phonebook;
 
-import java.io.IOException;
 import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Map;
 import java.util.TreeMap;
 
+import org.apache.commons.dbcp.ConnectionFactory;
+import org.apache.commons.dbcp.DriverManagerConnectionFactory;
+import org.apache.commons.dbcp.PoolableConnectionFactory;
+import org.apache.commons.dbcp.PoolingDataSource;
+import org.apache.commons.pool.impl.GenericObjectPool;
+
 
 public class AddressBook {
-	private Map<String,Contact> contactsByName;
+//	private Map<String,Contact> contactsByName;
+	private static PoolingDataSource poolingDataSource;
 	
-	public AddressBook(){
-		contactsByName = new TreeMap<String,Contact>();
-/*		try (Connection connection = DriverManager.getConnection(
-				"jdbc:mysql://phonebook3-resound.rhcloud.com:3306/phonebook3", "adminXU9chNa", "pyd5AUHSEa7H")){
-			Statement statement = connection.createStatement();
-			statement.executeQuery("INSERT INTO adresses VALUES('ins_first_to_db','332233','add from statement')");
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	public AddressBook() throws SQLException{
+		if (poolingDataSource==null){
+			String host = System.getenv("OPENSHIFT_MYSQL_DB_HOST");
+			String port = System.getenv("OPENSHIFT_MYSQL_DB_PORT");
+			String dbName = System.getenv("OPENSHIFT_APP_NAME");
+			String connectUri = "jdbc:mysql://"+host+":"+port+"/"+dbName;
+			String uname = System.getenv("OPENSHIFT_MYSQL_DB_USERNAME");
+			String passwd = System.getenv("OPENSHIFT_MYSQL_DB_PASSWORD");
+			
+			GenericObjectPool connectionPool = new GenericObjectPool(null);
+			ConnectionFactory connectionFactory = new DriverManagerConnectionFactory(connectUri, uname, passwd);
+			new PoolableConnectionFactory(connectionFactory, connectionPool, null, null, false, true);
+			poolingDataSource = new PoolingDataSource(connectionPool);
 		}
-	*/	
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		try (//Connection connection = DriverManager.getConnection(
+			 //	"jdbc:mysql://"+host+":3306/phonebook3", "adminXU9chNa", "pyd5AUHSEa7H")
+				Connection connection = poolingDataSource.getConnection()){
+			Statement statement = connection.createStatement();
+			statement.executeUpdate("INSERT INTO addresses VALUES('addfrst','111','cmnt')");
+		}
 	}
-	public AddressBook(String name, String number, String comment) throws IOException, SQLException{
+	public AddressBook(String name, String number, String comment) throws SQLException{
 		this();
 		addContact(name, number, comment);
 	}
-	public AddressBook(String name, String number) throws IOException, SQLException{
+	public AddressBook(String name, String number) throws SQLException{
 		this(name,number,"");
 	}
 
-	Map<String,Contact> getContacts(){
-		return contactsByName;
-	}
-	
-	public Contact getContact(String name) {
-		return contactsByName.get(name);
-	}
-
-	public void addContact(String name, String number, String comment) throws IOException{
-		contactsByName.put(name, new Contact(name, number, comment));
-
-/*		try (Connection connection = DriverManager.getConnection(
-				"jdbc:mysql://phonebook3-resound.rhcloud.com:3306/phonebook3", "adminXU9chNa", "pyd5AUHSEa7H")){
-			Statement statement = connection.createStatement();
-			statement.executeUpdate("INSERT INTO addresses VALUES('nm1','phn1','cmnt1')");
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	/**
+	 * Search in DB and return Contact for name
+	 * If DB has more then one record for name, then return the first record
+	 * @param name
+	 * @return Contact 
+	 */
+	public Contact getContact(String name) throws SQLException{
+		final String SQL = "SELECT name, number, comment FROM addresses WHERE name=?";
+		Contact contact = null;
+		try(Connection connection = poolingDataSource.getConnection();
+				PreparedStatement statement = connection.prepareStatement(SQL)){
+			statement.setString(1, name);
+			ResultSet result = statement.executeQuery();
+			while(result.next()){
+				contact = new Contact(name, result.getString("number"),	result.getString("comment"));
+				break;
+			}
 		}
-*/
-		
-		saveContactInformation();
+		return contact;
 	}
 	
-	public void addContact(String name, String number) throws IOException{
+	public Map<String,Contact> getContacts() throws SQLException{
+		final String SQL = "SELECT name, number, comment FROM adresses";
+		Map<String,Contact> contacts = new TreeMap<String, Contact>();
+		try(Connection connection = poolingDataSource.getConnection();
+				PreparedStatement statement = connection.prepareStatement(SQL)){
+			ResultSet result = statement.executeQuery();
+			while(result.next()){
+				Contact contact = new Contact(result.getString("name"), result.getString("number")
+						, result.getString("comment"));
+				contacts.put(contact.getName(), contact);
+			}
+		}
+		return contacts;
+	}
+
+	public void addContact(String name, String number, String comment)throws SQLException{
+		final String SQL = "INSERT INTO addresses(name,number,comment,) VALUES(?,?,?)";
+		try(Connection connection = poolingDataSource.getConnection();
+				PreparedStatement statement = connection.prepareStatement(SQL);) {
+			statement.setString(1, name);
+			statement.setString(2, number);
+			statement.setString(3, comment);
+			statement.executeUpdate();
+		}
+	}
+	
+	public void addContact(String name, String number)throws SQLException{
 		addContact(name, number, "");
 	}
 
-	public Contact remove(String name) throws IOException{
-		Contact contact = contactsByName.remove(name);
-		saveContactInformation();
-		return contact;
-		
+	public void remove(String name) throws SQLException{
+		final String SQL = "DELETE FROM addresses WHERE name=?";
+		try(Connection connection = poolingDataSource.getConnection();
+				PreparedStatement statement = connection.prepareStatement(SQL)){
+			statement.setString(1, name);
+			statement.executeUpdate();
+		}
 	}
 	
-	public boolean editContact(String oldName, String name, String number, String comment) throws IOException{
-		if(contactsByName.remove(oldName)==null)
-			return false;
-		addContact(name, number, comment);
-		saveContactInformation();
-		return true;
-	}
-	
-	private void saveContactInformation() throws IOException{
-		
+	public boolean editContact(String name, String number, String comment)throws SQLException{
+		boolean edit = false;
+		final String SQL = "UPDATE addresses SET number=?, comment=? WHERE name=?";
+		try(Connection connection = poolingDataSource.getConnection();
+				PreparedStatement statement = connection.prepareStatement(SQL);){
+			statement.setString(3, name);
+			statement.setString(1, number);
+			statement.setString(2, comment);
+			statement.executeUpdate();
+			edit = true;
+		}
+		return edit;
 	}
 }
--- src/main/java/resound/phonebook/AddressBookServlet.java
+++ src/main/java/resound/phonebook/AddressBookServlet.java
@@ -1,10 +1,8 @@
 package resound.phonebook;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.io.UnsupportedEncodingException;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -37,10 +35,10 @@ public class AddressBookServlet extends HttpServlet {
     @Override
     public void init(ServletConfig config) throws ServletException {
     	super.init(config);
-    	addressBook = new AddressBook();
     	try {
+        	addressBook = new AddressBook();
 			addressBook.addContact("first", "1234", "comm");
-		} catch (IOException e) {
+		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
@@ -49,41 +47,28 @@ public class AddressBookServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException {
 		handle(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException {
 		handle(request, response);
 	}
 
-	private void handle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-		request.setCharacterEncoding("utf-8");
-		String target = request.getRequestURI().substring(request.getContextPath().length());
-		
-		String host = System.getenv("OPENSHIFT_MYSQL_DB_HOST");
-		String port = System.getenv("OPENSHIFT_MYSQL_DB_PORT");
-
+	private void handle(HttpServletRequest request, HttpServletResponse response) throws ServletException{
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
-		} catch (ClassNotFoundException e1) {
+			request.setCharacterEncoding("utf-8");
+		} catch (UnsupportedEncodingException e) {
 			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		try (Connection connection = DriverManager.getConnection(
-				"jdbc:mysql://"+host+":3306/phonebook3", "adminXU9chNa", "pyd5AUHSEa7H")){
-			Statement statement = connection.createStatement();
-			statement.executeUpdate("INSERT INTO addresses VALUES('nm1','phn1','cmnt1')");
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			System.out.println(host+" "+port);
 			e.printStackTrace();
 		}
+		String target = request.getRequestURI().substring(request.getContextPath().length());
 
-		switch (target) {
+		try{
+			switch (target) {
 			case "/":
 				outputPage("index.jsp", request, response);
 				break;
@@ -107,10 +92,13 @@ public class AddressBookServlet extends HttpServlet {
 				break;
 			default:
 				break;
+			}		
+		}catch(SQLException e){
+			e.printStackTrace();
 		}
 	}
 	
-	private void handleView(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+	private void handleView(HttpServletRequest request, HttpServletResponse response) throws ServletException, SQLException{
 		Map<String, String> numbers = new HashMap<String, String>();
 		Map<String, String> comments = new HashMap<String, String>();
 		for(Map.Entry<String, Contact> entry : addressBook.getContacts().entrySet()){
@@ -122,7 +110,7 @@ public class AddressBookServlet extends HttpServlet {
 		outputPage("view.jsp", request, response);
 	}
 	
-	private void handleAdd(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+	private void handleAdd(HttpServletRequest request, HttpServletResponse response) throws ServletException, SQLException{
 		if(request.getParameter("name")==null){
 			request.setAttribute("name", "");
 			request.setAttribute("number", "");
@@ -145,13 +133,12 @@ public class AddressBookServlet extends HttpServlet {
 		}
 	}
 	
-	private void handleEdit(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+	private void handleEdit(HttpServletRequest request, HttpServletResponse response) throws ServletException, SQLException{
 		String name = request.getParameter("name");
 		if(request.getParameter("edited")!=null){
-			String eName = (String) request.getParameter("name");
 			String eNumber = (String) request.getParameter("number");
 			String eComment = (String) request.getParameter("comment");
-			addressBook.editContact(name, eName, eNumber, eComment);
+			addressBook.editContact(name, eNumber, eComment);
 			handleView(request, response);
 		}else if(name!=null){ 
 			Contact contact = addressBook.getContacts().get(name);
@@ -165,15 +152,16 @@ public class AddressBookServlet extends HttpServlet {
 		
 	}
 	
-	private void handleRemove(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+	private void handleRemove(HttpServletRequest request, HttpServletResponse response) throws ServletException, SQLException{
 		String name = request.getParameter("name");
-		Contact contact = addressBook.getContact(name);
+		Contact contact = null;
+		contact = addressBook.getContact(name);
 		addressBook.remove(name);
 		request.setAttribute("message", "Removed: "+contact);
 		handleView(request, response);
 	}
 	
-	private void handleAuth(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+	private void handleAuth(HttpServletRequest request, HttpServletResponse response) throws ServletException{
 		String login = request.getParameter("login");
 		String pass = request.getParameter("password");
 		if(login!=null & pass!=null){
@@ -192,13 +180,23 @@ public class AddressBookServlet extends HttpServlet {
 		}
 	}
 	
-	private void handleLogout(HttpServletRequest request, HttpServletResponse response) throws IOException {
+	private void handleLogout(HttpServletRequest request, HttpServletResponse response){
 		request.getSession().invalidate();
-		response.sendRedirect(request.getContextPath());
+		try {
+			response.sendRedirect(request.getContextPath());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
-	private void outputPage(String JSPName, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+	private void outputPage(String JSPName, HttpServletRequest request, HttpServletResponse response) throws ServletException{
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/jsps/" + JSPName);
-		dispatcher.forward(request, response);
+		try {
+			dispatcher.forward(request, response);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
